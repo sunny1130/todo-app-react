398e3d61147162b55d7b56fd7b93a794e39c9185
sunny1130 <sunmeet.singh1130@gmail.com>
2024-03-09 18:35:03 -0800

    hardcoded login functionality

--- src/components/todo/ToDoApp.jsx
+++ src/components/todo/ToDoApp.jsx
@@ -14,14 +14,45 @@ function Login(){
 
     const [username,setUserName] = useState("")
     const [password,setPassword] = useState("")
+    const [successLogin,setSuccessLogin] = useState(false)
+    const [unsuccessLogin,setUnSuccessLogin] = useState(false)
 
     function handlePasswordChange(event){
         setPassword(event.target.value)
     }
 
+    function handleLogin(){
+        //Check Login Credentials and Perform Next Steps
+        //TO-DO
+        //Hard Coding for now
+        if(username === 'a' && password === 'a'){
+            setSuccessLogin(true)
+            setUnSuccessLogin(false)
+        }else{
+            setSuccessLogin(false)
+            setUnSuccessLogin(true)
+        }
+    }
+
+    function SuccessfullLogin(){
+        if(successLogin){
+            return <div className="SuccessLogin">Authenticated Successfully</div>
+        }
+        return null
+    }
+
+    function UnSuccessfullLogin(){
+        if(unsuccessLogin){
+            return <div className="ErrorLogin">Authentication Failed</div>
+        }
+        return null
+    }
+
     return(
         <div className="Login">
             <div className="LoginForm">
+                <SuccessfullLogin/>
+                <UnSuccessfullLogin/>
                 <div>
                     <label>User Name</label>
                     <input type="text" name="username" value={username} onChange={(event)=>setUserName(event.target.value)}/>
@@ -31,7 +62,7 @@ function Login(){
                     <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" >Login</button>
+                    <button type="button" onClick={handleLogin}>Login</button>
                 </div>
             </div>
         </div>
